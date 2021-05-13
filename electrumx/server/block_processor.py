6716fe84191140ea8e592bd11e3892b5e9e83d6a
kralverde <2b2tminecrafter2@gmail.com>
2021-05-13 19:48:19 -0400

    Support for pubkey scripts

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -8,6 +8,7 @@
 
 '''Block prefetcher and chain processor.'''
 
+import logging
 
 import asyncio
 import time
@@ -510,8 +511,10 @@ class BlockProcessor:
                     pubkey = txout.script[1:pubkey_len + 1]
                     try:
                         addr = public_key_to_address(pubkey, self.coin.P2PKH_VERBYTE)
+                        print('found pubkey addr: {}'.format(addr))
                         hashX = self.coin.address_to_hashX(addr)
                     except:
+                        logging.exception("Here's the pubkey error:")
                         pass
 
                 append_hashX(hashX)
