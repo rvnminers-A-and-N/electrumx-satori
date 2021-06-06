3be15ddaf165610b0bd636160d63e3c29621d7b1
kralverde <electrum.raven@gmail.com>
2021-06-05 22:00:59 -0400

    script parsing work

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -571,8 +571,8 @@ class BlockProcessor:
                     raise Exception('Unknown script: {}'.format(txout.pk_script.hex()))
 
                 print(txout.pk_script.hex())
-                print(txout.pk_script[:ops[ctr][1]].hex())
-                script_hashX(txout.pk_script[:ops[ctr][1]])
+                print(txout.pk_script[:ops[ctr][1]-1].hex())
+                script_hashX(txout.pk_script[:ops[ctr][1]-1])
                 raise Exception()
 
                 # Add UTXO info to the database
